e586f28da448186d32d871c77cc4c1f46687829d
raxbg <crownandthering@gmail.com>
2013-07-01 18:18:57 +0300

    Bug fix for the playlist detection
    Basicly the same, just remove the whitespace from the innerHTML before
    checking its length

--- contentscript.js
+++ contentscript.js
@@ -4,7 +4,7 @@ var playlist = false;
 if (document.getElementsByTagName('video').length > 0) {
 	videoObj = document.getElementsByTagName('video')[0];
 	
-	if (document.getElementById('playlist').innerHTML.length > 0) {
+	if (document.getElementById('playlist').innerHTML.replace(/\s*/g, '').length > 0) {
 		playlist = true;
 	}
 
