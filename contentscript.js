45c5586ba173800a6541f548e89b3fae2609e0a1
raxbg <crownandthering@gmail.com>
2013-07-02 02:03:51 +0300

    Support for playlist shortcuts even if the videos are not HTML5

--- contentscript.js
+++ contentscript.js
@@ -1,24 +1,30 @@
 var videoObj = null;
 var playlist = false;
+var tabRegistered = false;
 
 if (document.getElementsByTagName('video').length > 0) {
 	videoObj = document.getElementsByTagName('video')[0];
-	
-	if (document.getElementById('playlist').innerHTML.replace(/\s*/g, '').length > 0) {
-		playlist = true;
-	}
 
 	videoObj.addEventListener('pause', function() { chrome.runtime.sendMessage({playerStateChange: true, state: 'paused'}, function(response){}); });
 	videoObj.addEventListener('play', function() { chrome.runtime.sendMessage({playerStateChange: true, state: 'playing'}, function(response){}); });
 	videoObj.addEventListener('ended', function() { chrome.runtime.sendMessage({playerStateChange: true, state: 'ended'}, function(response){}); });
 
 	chrome.runtime.sendMessage({registerTab: true, state: 'paused'}, function(response){});
-
-	chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
-		if(typeof msg.cmd != 'undefined' && videoObj != null) {
-			eval(msg.cmd);
-		}
-
-		sendResponse();
-	});
+	tabRegistered = true;
 }
+
+if (document.getElementById('playlist').innerHTML.replace(/\s*/g, '').length > 0) {
+	playlist = true;
+
+	if (!tabRegistered) {
+		chrome.runtime.sendMessage({playerStateChange: true, state: 'playing'}, function(response){});
+	}
+}
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
+	if(typeof msg.cmd != 'undefined' && (videoObj != null || playlist)) {
+		eval(msg.cmd);
+	}
+
+	sendResponse();
+});
